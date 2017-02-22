5c5236460788cff46ac19e5f9650c821dc48e05a
skrusty <ben@xdev.net>
2017-02-22 10:45:10 +0000

    Added nuspec file to repo, updated everything to reflect changes for new release. 1.2.0

--- Asterisk.2013/Asterisk.NET/Properties/AssemblyInfo.cs
+++ Asterisk.2013/Asterisk.NET/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AsterNET")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("abe98502-ea83-4b04-98c3-ffe3eabe06b0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
