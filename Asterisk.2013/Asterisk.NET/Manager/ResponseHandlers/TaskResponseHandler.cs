2cd025d2f90bca9f5497edc2ba24775f8990ec3b
ddyner <ddyner@yandex.ru>
2017-04-06 14:39:35 +0300

    Add files via upload

---
+++ Asterisk.2013/Asterisk.NET/Manager/ResponseHandlers/TaskResponseHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using AsterNET.Manager.Action;
+
+namespace AsterNET.Manager.Response
+{
+    internal class TaskResponseHandler : IResponseHandler
+    {
+        public TaskResponseHandler(ManagerAction action)
+        {
+            TaskCompletionSource = new TaskCompletionSource<ManagerResponse>();
+            Action = action;
+        }
+
+        public TaskCompletionSource<ManagerResponse> TaskCompletionSource { get; }
+
+        public ManagerAction Action { get; }
+
+        public int Hash { get; set; }
+
+        public void HandleResponse(ManagerResponse response)
+        {
+            TaskCompletionSource.TrySetResult(response);
+        }
+
+        public void Free() { }
+    }
+}
