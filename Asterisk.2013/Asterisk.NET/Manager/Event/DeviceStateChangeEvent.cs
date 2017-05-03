d63aa5e614c75c62ba82f3f43e1e0c404196ab89
DaveTTS <david@dglj.com>
2017-05-03 15:55:13 +0100

    Add files via upload

---
+++ Asterisk.2013/Asterisk.NET/Manager/Event/DeviceStateChangeEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace AsterNET.Manager.Event
+{
+
+    /// <summary>
+    ///     Raised when a device state changes.<br />
+    ///     This differs from the ExtensionStatus event because this event is raised for all device state changes, not only for changes that affect dialplan hints.
+    /// </summary>
+    public class DeviceStateChangeEvent : ManagerEvent
+    {
+        public DeviceStateChangeEvent(ManagerConnection source)
+            : base(source)
+        {
+        }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Asterisk.2013/Asterisk.NET/Manager/Event/SuccessfulAuthEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace AsterNET.Manager.Event
+{
+
+    /// <summary>
+    ///     Raised when a request successfully authenticates with a service..<br />
+    /// </summary>
+    public class SuccessfulAuthEvent : ManagerEvent
+    {
+        public SuccessfulAuthEvent(ManagerConnection source)
+            : base(source)
+        {
+        }
+
+        public string Status { get; set; }
+    }
+}
