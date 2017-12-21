d055641b18795e6ab95f9745414e737d6154298b
Deantwo <dean@samsig.dk>
2017-12-21 16:02:23 +0100

    Update QueueCallerJoinEvent.cs
    Inheritance from JoinEvent.

--- Asterisk.2013/Asterisk.NET/Manager/Event/QueueCallerJoinEvent.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Event/QueueCallerJoinEvent.cs
@@ -1,13 +1,25 @@
 namespace AsterNET.Manager.Event
 {
-    /// <summary>
-    /// A QueueCallerJoinEvent is triggered when a channel joins a queue.<br/>
-    /// </summary>
-    public class QueueCallerJoinEvent : QueueEvent
-    {
-        public string Position { get; set; }
-
-        public QueueCallerJoinEvent(ManagerConnection source)
+	/// <summary>
+	/// A QueueCallerJoinEvent is triggered when a channel joins a queue.<br/>
+	/// </summary>
+	public class QueueCallerJoinEvent : JoinEvent
+	{
+		// "Channel" in ManagerEvent.cs
+		
+		// "Queue" in QueueEvent.cs
+		
+		/// <summary>
+		///     Get/Set the Caller*ID number of the channel that joined the queue if set.
+		///     If the channel has no caller id set "unknown" is returned.
+		/// </summary>
+		public string CallerIDNum { get; set; }
+		
+		// "CallerIdName" in JoinEvent.cs
+		
+		// "Position" in JoinEvent.cs
+		
+		public QueueCallerJoinEvent(ManagerConnection source)
 			: base(source)
 		{
 		}
