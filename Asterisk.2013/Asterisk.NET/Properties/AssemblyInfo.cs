2aaeeb2a40c49d51c18e189d07dc8d9a1bd3818a
Deantwo <dean@samsig.dk>
2018-07-10 16:15:42 +0200

    AsterNET 1.3 release (#156)

--- Asterisk.2013/Asterisk.NET/Properties/AssemblyInfo.cs
+++ Asterisk.2013/Asterisk.NET/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("abe98502-ea83-4b04-98c3-ffe3eabe06b0")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
