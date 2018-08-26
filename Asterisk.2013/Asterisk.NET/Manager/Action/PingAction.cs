7ed06fea389923a5b633df8dcaeff1bda0e785cc
Craig Roberts <craig.roberts@crdevel.com>
2018-08-26 12:07:24 -0400

    adding references in summaries to asterisk docs

--- Asterisk.2013/Asterisk.NET/Manager/Action/PingAction.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Action/PingAction.cs
@@ -1,8 +1,10 @@
 namespace AsterNET.Manager.Action
 {
     /// <summary>
-    ///     The PingAction will ellicit a 'Pong' response, it is used to keep the manager
+    ///     The PingAction will elicit a 'pong' response, it is used to keep the manager
     ///     connection open and performs no operation.
+    ///     
+    ///     See <see target="_blank"  href="https://wiki.asterisk.org/wiki/display/AST/Asterisk+16+ManagerAction_Ping">https://wiki.asterisk.org/wiki/display/AST/Asterisk+16+ManagerAction_Ping</see>
     /// </summary>
     public class PingAction : ManagerAction
     {
--- Asterisk.2013/Asterisk.NET/Manager/Action/QueueResetAction.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Action/QueueResetAction.cs
@@ -1,16 +1,19 @@
 ﻿namespace AsterNET.Manager.Action
 {
+    /// <summary>
+    ///     Reset queue statistics.
+    /// </summary>
     public class QueueResetAction : ManagerAction
     {
         /// <summary>
-        ///     Reset queue statistics.
+        ///     Creates a new empty <see cref="QueueResetAction"/>.
         /// </summary>
         public QueueResetAction()
         {
         }
 
         /// <summary>
-        ///     Reset queue statistics.
+        ///     Creates a new <see cref="QueueResetAction"/> using the given queue.
         /// </summary>
         /// <param name="queue"></param>
         public QueueResetAction(string queue)
@@ -18,11 +21,17 @@
             Queue = queue;
         }
 
+        /// <summary>
+        ///     Get the name of this action, i.e. "QueueReset".
+        /// </summary>
         public override string Action
         {
             get { return "QueueReset"; }
         }
 
+        /// <summary>
+        /// Gets or sets the queue.
+        /// </summary>
         public string Queue { get; set; }
     }
 }
