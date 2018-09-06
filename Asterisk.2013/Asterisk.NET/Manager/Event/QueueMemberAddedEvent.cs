f1261526fe4d6ded5afee2a56ed5f1d49a5e4a9f
Craig Roberts <craig.roberts@crdevel.com>
2018-09-06 19:03:32 -0400

    summary updates (manager > event)

--- Asterisk.2013/Asterisk.NET/Manager/Event/QueueMemberAddedEvent.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Event/QueueMemberAddedEvent.cs
@@ -1,18 +1,16 @@
 namespace AsterNET.Manager.Event
 {
 	/// <summary>
-	/// A QueueMemberAddedEvent is triggered when a queue member is added to a queue.<br/>
-	/// It is implemented in apps/app_queue.c.<br/>
-	/// <para>
-	/// <b>Available since : </b> <see href="http://www.voip-info.org/wiki/view/Asterisk+v1.2" target="_blank" alt="Asterisk 1.2 wiki docs">Asterisk 1.2</see>.<br/>
-	/// </para>
+	///     A QueueMemberAddedEvent is triggered when a queue member is added to a queue.<br/>
+	///     It is implemented in apps/app_queue.c.<br/>
+	///     <b>Available since : </b> <see href="http://www.voip-info.org/wiki/view/Asterisk+v1.2" target="_blank" alt="Asterisk 1.2 wiki docs">Asterisk 1.2</see>.
 	/// </summary>
 	public class QueueMemberAddedEvent : AbstractQueueMemberEvent
 	{
 		/// <summary>
-		/// Creates a new QueueMemberAddedEvent
+		///     Creates a new <see cref="QueueMemberAddedEvent"/>
 		/// </summary>
-		/// <param name="source">ManagerConnection passed through in the event.</param>
+		/// <param name="source"><see cref="ManagerConnection"/></param>
 		public QueueMemberAddedEvent(ManagerConnection source)
 			: base(source)
 		{
