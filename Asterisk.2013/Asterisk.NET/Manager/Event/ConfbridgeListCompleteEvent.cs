ddd9b6086bdf40b434341224aa24c4a960c0deb6
Craig Roberts <craig.roberts@crdevel.com>
2018-09-06 19:12:06 -0400

    add missing fields and summaries

--- Asterisk.2013/Asterisk.NET/Manager/Event/ConfbridgeListCompleteEvent.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Event/ConfbridgeListCompleteEvent.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace AsterNET.Manager.Event
 {
+    /// <summary>
+    ///     An ConfbridgeListCompleteEvent is triggered after the state of all Confbridges has been
+    ///     reported in response to an ConfbridgeListAction.
+    ///     
+    ///     See <see target="_blank"  href="https://wiki.asterisk.org/wiki/display/AST/ConfBridge+AMI+Actions">https://wiki.asterisk.org/wiki/display/AST/ConfBridge+AMI+Actions</see>
+    /// </summary>
+    /// /// <seealso cref="ConfbridgeListEvent" />
     public class ConfbridgeListCompleteEvent : ResponseEvent
     {
+        /// <summary>
+        ///     Creates a new <see cref="ConfbridgeListCompleteEvent"/> using the given <see cref="ManagerConnection"/>.
+        /// </summary>
+        /// <param name="source"></param>
         public ConfbridgeListCompleteEvent(ManagerConnection source)
 			: base(source)
 		{
 		}
     }
 }
+
--- Asterisk.2013/Asterisk.NET/Manager/Event/ConfbridgeListRoomsCompleteEvent.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Event/ConfbridgeListRoomsCompleteEvent.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace AsterNET.Manager.Event
 {
+    /// <summary>
+    /// <summary>
+    ///     An ConfbridgeListRoomsCompleteEvent is triggered after the state of all ConfBridgeRooms has been
+    ///     reported in response to an ConfbridgeListRoomsAction.
+    ///     
+    ///     See <see target="_blank"  href="https://wiki.asterisk.org/wiki/display/AST/ConfBridge+AMI+Actions">https://wiki.asterisk.org/wiki/display/AST/ConfBridge+AMI+Actions</see>
+    /// </summary>
+    /// <seealso cref="ConfbridgeListRoomsEvent" />
     public class ConfbridgeListRoomsCompleteEvent : ResponseEvent
     {
-
+        /// <summary>
+        ///     Creates a new <see cref="ConfbridgeListRoomsCompleteEvent"/> using the given <see cref="ManagerConnection"/>.
+        /// </summary>
+        /// <param name="source"></param>
         public ConfbridgeListRoomsCompleteEvent(ManagerConnection source)
 			: base(source)
 		{
