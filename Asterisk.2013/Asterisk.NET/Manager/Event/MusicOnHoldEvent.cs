5b2dece94547843338d18544df65847424ab6dfe
Craig Roberts <craig.roberts@crdevel.com>
2018-09-06 21:59:58 -0400

    re-add musiconholdevent
    re-add musiconholdevent
    add summaries

---
+++ Asterisk.2013/Asterisk.NET/Manager/Event/MusicOnHoldEvent.cs
@@ -0,0 +1,45 @@
+﻿namespace AsterNET.Manager.Event
+{
+    /// <summary>
+    ///     A MusicOnHoldEvent is triggered when music on hold starts or stops on a channel.<br/>
+    ///     It is implemented in res/res_musiconhold.c.<br/>
+    ///     Available since Asterisk 1.6
+    /// </summary>
+    public class MusicOnHoldEvent : ManagerEvent 
+	{
+        /// <summary>
+        ///     Creates a new <see cref="MusicOnHoldEvent" />.
+        /// </summary>
+        /// <param name="source"><see cref="ManagerConnection"/></param>
+        public MusicOnHoldEvent(ManagerConnection source) : base(source)
+		{
+		}
+
+		/// <summary>
+		///     Gets or sets the state.
+		/// </summary>
+		public string State { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the class.<br/>
+        ///     The class of music being played on the channel.
+        /// </summary>
+        public string Class { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the account code.
+        /// </summary>
+        public string AccountCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the language.
+        /// </summary>
+        public string Language { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Linked Id<br/>
+        ///     UniqueId of the oldest channel associated with this channel.
+        /// </summary>
+        public string LinkedId { get; set; }
+    }
+}
--- Asterisk.2013/Asterisk.NET/Manager/Event/MusicOnHoldStopEvent.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Event/MusicOnHoldStopEvent.cs
@@ -13,21 +13,67 @@
         public MusicOnHoldStopEvent(ManagerConnection source) : base(source)
 		{
 		}
-        
+
+        /// <summary>
+        ///     Gets or sets the channel state.<br/>
+        ///     A numeric code for the channel's current state, related to ChannelStateDesc
+        /// </summary>
         public string ChannelState { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the channel state description.
+        /// </summary>
         public string ChannelStateDesc { get; set; }
-        
+
+        /// <summary>
+        ///     Gets or sets the Caller*ID number.
+        /// </summary>
         public string CallerIDNum { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Caller*ID name.
+        /// </summary>
         public string CallerIDName { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the connected line number.
+        /// </summary>
         public string ConnectedLineNum { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the connected line name.
+        /// </summary>
         public string ConnectedLineName { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the language.
+        /// </summary>
         public string Language { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the account code.
+        /// </summary>
         public string AccountCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the context.
+        /// </summary>
         public string Context { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the exten.
+        /// </summary>
         public string Exten { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the priority.
+        /// </summary>
         public string Priority { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the Linked Id
+        ///     UniqueId of the oldest channel associated with this channel.
+        /// </summary>
+        public string LinkedId { get; set; }
     }
 }
