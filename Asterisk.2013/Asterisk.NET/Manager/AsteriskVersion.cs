acd9e885cc454009b112836741fa515cc8772e23
Deantwo <2676134+Deantwo@users.noreply.github.com>
2020-01-21 10:47:48 +0100

    Extended AsteriskVersion Enum
    Added an "Unknown" option.
    Added Asterisk version up to 17.
    Added a "Newer" option.

--- Asterisk.2013/Asterisk.NET/Manager/AsteriskVersion.cs
+++ Asterisk.2013/Asterisk.NET/Manager/AsteriskVersion.cs
@@ -1,7 +1,11 @@
 namespace AsterNET.Manager
 {
+    /// <summary>
+    ///     Asterisk Version
+    /// </summary>
     public enum AsteriskVersion
     {
+        Unknown = 0,
         ASTERISK_1_0 = 10,
         ASTERISK_1_2 = 12,
         ASTERISK_1_4 = 14,
@@ -10,6 +14,11 @@ namespace AsterNET.Manager
         ASTERISK_10 = 100,
         ASTERISK_11 = 110,
         ASTERISK_12 = 120,
-        ASTERISK_13 = 130
+        ASTERISK_13 = 130,
+        ASTERISK_14 = 140,
+        ASTERISK_15 = 150,
+        ASTERISK_16 = 160,
+        ASTERISK_17 = 170,
+        ASTERISK_Newer = 999
     }
 }
