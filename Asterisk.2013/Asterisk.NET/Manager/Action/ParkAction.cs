ad03a5017f8256dea189ddc912f5ad3f1569e082
Fazrin <nirzaf@gmail.com>
2020-02-07 08:21:29 +0530

    Park Action Updated...
    Park Action Update Preserving the Backward Compatibility,

--- Asterisk.2013/Asterisk.NET/Manager/Action/ParkAction.cs
+++ Asterisk.2013/Asterisk.NET/Manager/Action/ParkAction.cs
@@ -6,28 +6,42 @@ namespace AsterNET.Manager.Action
     /// </summary>
     public class ParkAction : ManagerAction
     {
+
         /// <summary>
         ///     Creates a new ParkAction.
         /// </summary>
-        /// <param name="callerChannel">Set the Channel which should be parked</param>
-        /// <param name="calleeChannel">Set the Channel where the Call will end up after the timeout is reached.</param>
-        /// <param name="timeout">Timeout in milliseconds, after timeout is reached call will come back to channel2</param>
-        /// <param name="announceChannel">If specified, then this channel will receive an announcement when Channel is parked if AnnounceChannel is in a state where it can receive announcements (AnnounceChannel must be bridged). AnnounceChannel has no bearing on the actual state of the parked call.</param>
-        public ParkAction(string callerChannel, string calleeChannel, string announceChannel, string timeout)
+        /// <param name="channel"></param>
+        /// <param name="channel2"></param>
+        /// <param name="timeout"></param>
+        public ParkAction(string channel, string channel2, string timeout)
         {
-            Channel = callerChannel;
-            TimeoutChannel = calleeChannel;
-            AnnounceChannel = announceChannel;
-            Timeout = timeout;
+            this.Channel = channel;
+            this.Channel2 = channel2;
+            this.Timeout = timeout;
         }
 
         /// <summary>
         ///     Creates a new ParkAction.<br />
         /// </summary>
+        /// <param name="channel">Set the Channel which should be parked</param>
+        /// <param name="channel2">Set the Channel where the Call will end up after the timeout is reached.</param>
+        /// <param name="timeout">Timeout in msec, after timeout is reached call will come back to channel2</param>
+        /// <param name="parkinglot">Set the Parking lot.</param>
+        public ParkAction(string channel, string channel2, string timeout, string parkinglot)
+        {
+            this.Channel = channel;
+            this.Channel2 = channel2;
+            this.Timeout = timeout;
+            this.Parkinglot = parkinglot;
+        }
+
+        /// <summary>
+        ///     Creates a new ParkAction with announce channel .<br />
+        /// </summary>
         /// <param name="callerChannel">Set the Channel which should be parked</param>
         /// <param name="calleeChannel">Set the Channel where the Call will end up after the timeout is reached.</param>
         /// <param name="timeout">Timeout in milliseconds, after timeout is reached call will come back to channel2</param>
-        /// <param name="announceChannel">If specified, then this channel will receive an announcement when Channel is parked if AnnounceChannel is in a state where it can receive announcements (AnnounceChannel must be bridged). AnnounceChannel has no bearing on the actual state of the parked call.</param>
+        /// <param name="announceChannel">If specified, then this channel will receive an announcement when Channel is parked</param>
         /// <param name="parkinglot">Set the Parking lot.</param>
         public ParkAction(string callerChannel, string calleeChannel, string announceChannel, string timeout, string parkinglot)
         {
@@ -50,6 +64,11 @@ namespace AsterNET.Manager.Action
         ///     Set the Channel which should be parked
         /// </summary>
         public string Channel { get; set; }
+
+        /// <summary>
+        ///     Set the Channel where the Call will end up after the timeout is reached.
+        /// </summary>
+        public string Channel2 { get; set; }
 
         /// <summary>
         ///     Set the Channel where the Call will end up after the timeout is reached.
