74b08e52786f10fbc51ba2c31d8f8ef5c6f20dcb
Hugo Castro de Deco (WorkDesktop) <hugodeco@sufficit.com.br>
2021-02-18 00:36:56 -0300

    inclusão de framework;

--- Asterisk.2013/Asterisk.NET/FastAGI/AGIRequest.cs
+++ Asterisk.2013/Asterisk.NET/FastAGI/AGIRequest.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Linq;
 
 namespace AsterNET.FastAGI
